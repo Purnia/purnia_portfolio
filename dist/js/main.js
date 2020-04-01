467dad5c6d0f8af38d23174524e0e101b5f7ad0f
Purni <purnia92@gmail.com>
2020-03-31 23:09:47 -0500

    changing files

---
+++ dist/js/main.js
@@ -0,0 +1,35 @@
+// select DOM items
+var menuBtn = document.querySelector('.menu-btn');
+var menu = document.querySelector('.menu');
+var menuNav = document.querySelector('.menu-nav');
+var menuBranding = document.querySelector('.menu-branding');
+var navItems = document.querySelectorAll('.nav-item');
+
+//set initial state of menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
+
+    // set many state
+    showMenu = true;
+    } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+
+    //set many state
+    showMenu = false;
+
+
+    }
+}
